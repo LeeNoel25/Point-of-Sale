e16a20170371c1576a555c6fec0e54c7e4daeee4
LeeNoel <112954324+LeeNoel25@users.noreply.github.com>
2023-05-13 16:06:56 +0800

    Outlined Nav Bar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,19 @@
+import { HStack, Image, List, Link as ChakraLink } from '@chakra-ui/react';
+import { Link as RouterLink } from 'react-router-dom';
+import logo from '../assets/logo.png';
+
+const NavBar = () => {
+    return (
+        <List>
+        <HStack padding='10px'> 
+            <ChakraLink as={RouterLink} to="/">
+                <Image src={logo} boxSize='60px' alt="logo" />
+            </ChakraLink>
+            <ChakraLink as={RouterLink} to="/new">Add New Fruit</ChakraLink>
+        </HStack>
+        </List>
+    )
+}
+
+
+export default NavBar;
